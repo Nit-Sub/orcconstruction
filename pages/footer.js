7a949601fe8790e37a7eda28822734a385173bc5
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-22 22:01:16 +0545

    Update footer.js

--- pages/footer.js
+++ pages/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>©2022 DuruThemes. All rights reserved.</div>
+  <footer>©2022 DuruThemes. All rights reserved.</footer>
   )
 }
 
