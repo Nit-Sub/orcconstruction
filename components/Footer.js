1afb0d83b8223951fe4d9e2ca572bacbe67a5d79
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-03-03 15:21:26 +0545

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,67 @@
 import React from 'react'
 import styles from '@/styles/Footer.module.css'
+import FooterCard from './FooterCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLaptop } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot} from '@fortawesome/free-solid-svg-icons';
+import { faComment} from '@fortawesome/free-solid-svg-icons';
+
 
 const Footer = () => {
-  return (
-    <div className={styles.main}>
-        <div className={styles.description}>
-            <hr className={styles.hr}/>
-            <FontAwesomeIcon icon={ faPhone} className={styles.font}/>
-            
-
+    return (
+        <div className={styles.main}>
+            <div className={styles.description}>
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <FooterCard title="Call us" contact="+1 203-333-4444" />
+                    </div> <div className={styles.column}>
+                        <FooterCard title="Call us" contact="+1 203-333-4444" />
+                    </div> <div className={styles.column}>
+                        <FooterCard title="Call us" contact="+1 203-333-4444" />
+                    </div>
+                </div>
+                <div className={styles.rows}>
+                    <div className={styles.column}>
+                        <h3 className={styles.h3}>About Norc.</h3>
+                        <p className={styles.p}>
+                            Quisque imperdiet sapien porttito the bibendum sellentesque the commodo erat acar accumsa lobortis, enim diam the nesuen.
+                        </p>
+                        <div className={styles.rows}>
+                            <div className={styles.columnfooter}>
+                                < div className={styles.footericon}>
+                                    <FontAwesomeIcon icon={faLaptop} />
+                                </div>
+                            </div><div className={styles.columnfooter}>
+                                < div className={styles.footericon}>
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </div>
+                            </div><div className={styles.columnfooter}>
+                                < div className={styles.footericon}>
+                                    <FontAwesomeIcon icon={faLocationDot} />
+                                </div>
+                            </div>
+                            <div className={styles.columnfooter}>
+                                < div className={styles.footericon}>
+                                    <FontAwesomeIcon icon={faComment} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.column}>
+                        <h3 className={styles.h3}>Quick Links</h3>
+                        <ul>
+                            <li >About</li>
+                            <li>Service</li>
+                            <li>Project</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                    <div className={styles.column}>A </div>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
--- components/FooterCard.js
+++ components/FooterCard.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import styles from '@/styles/FooterCard.module.css'
 
-const FooterCard = () => {
-  return (
-    <div>FooterCard</div>
-  )
+const FooterCard = ({ title, contact }) => {
+    return (
+        <>
+            <div className={styles.row}>
+                <div className={styles.column}>
+                    <div className={styles.iconcard}>
+                        <FontAwesomeIcon icon={faPhone} className={styles.font} />
+                        </div>
+                        </div>
+                <div className={styles.column}>
+                    <div className={styles.callus}>
+                        <h6>{title}</h6>
+                        <p>{contact}</p>
+                        </div>
+                        
+                    </div>
+                    <div className={styles.divider}></div>
+                   
+                </div>
+            
+        </>
+    )
 }
 
 export default FooterCard
